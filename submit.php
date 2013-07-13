9e9414f04b9cd462706ca6e58069ac5eae3f3345
Peter Jewicz <Peterjewicz@hotmail.com>
2013-07-13 12:03:53 -0500

    Security Change: Select Prepated
    Change Select to Prepared statement for more security.

--- submit.php
+++ submit.php
@@ -7,11 +7,16 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $confirmed = 0;
 // Check to see if email is already in use
-$sql = "SELECT * FROM users WHERE email='$email'";
-$result=$connection->query($sql);
-$row = mysqli_num_rows($result);
+$sql = "SELECT * FROM users WHERE email = ?";
+$stmt = $connection->prepare($sql);
+$stmt->bind_param('s',  $email);
+$stmt->execute();
+$result = $stmt->fetch();
+//$row = mysqli_num_rows($result);
+$stmt->close(); 
 
-if($row==0) // if no email found insert
+
+if($result==0) // if no email found insert
 {
 echo("email was not found");
 //insert email and name into database
@@ -26,7 +31,7 @@ $stmt->close();
 $subject = "Please Confirm Your Email";
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$message = "please confrim your email\n\n";
+$message = "please confrim mother fucker\n\n";
 $message .="<a href='http://localhost/Auto_Responder/confirm.php?email=$email'> click here </a>";
 
 mail($email, $subject, $message, $headers);
